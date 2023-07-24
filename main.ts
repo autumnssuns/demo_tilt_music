82a9a84cf45f3dcb00858a38a979888575dfe568
Dan Tran <67458114+autumnssuns@users.noreply.github.com>
2023-07-24 11:30:42 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+let prevY = 0
+let prevX = 0
+let x = 2
+let y = 2
+music.setVolume(75)
+basic.forever(function () {
+    basic.clearScreen()
+    led.plot(x, y)
+    prevX = x
+    prevY = y
+    if (x < 4 && input.rotation(Rotation.Roll) > 0 || x > 0 && input.rotation(Rotation.Roll) < 0) {
+        x = x + input.rotation(Rotation.Roll) / 60
+    }
+    if (y < 4 && input.rotation(Rotation.Pitch) > 0 || y > 0 && input.rotation(Rotation.Pitch) < 0) {
+        y = y + input.rotation(Rotation.Pitch) / 60
+    }
+    if (prevX != x || prevY != y) {
+        music.play(music.tonePlayable((y + 1) * 150 + x * 30, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
+    }
+})
